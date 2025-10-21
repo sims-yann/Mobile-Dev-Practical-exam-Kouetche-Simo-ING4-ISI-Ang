1efa79dd235eb65f5013019d53446196b7752a9d
sims-yann <simoyann2004@gmail.com>
2025-10-21 12:05:11 +0100

    added news display rendering

--- app/index.tsx
+++ app/index.tsx
@@ -1,6 +1,8 @@
 import React, {useState} from "react";
 import { Text, View, StyleSheet, TextInput, Button } from "react-native";
+import { FlatList } from "react-native-gesture-handler";
 import { SafeAreaView } from 'react-native-safe-area-context';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
 export default function Index() {
   const [allnews, setAllNews] = useState<string[]>([]);
@@ -13,7 +15,14 @@ export default function Index() {
     setNews("");
   };
 
+  const renderItem = ({ item }: { item: string }) => (
+    <View style={styles.taskRow}>
+      <Text style={styles.taskText}>{item}</Text>
+    </View>
+  );
+
   return (
+    <GestureHandlerRootView style={{ flex: 1 }}>
     <SafeAreaView style={styles.container}>
       <View>
         <Text style={styles.header}>News update App</Text>
@@ -28,7 +37,18 @@ export default function Index() {
         </View>
         <Text>{allnews}</Text>
       </View>
+
+      {allnews.length === 0 ? (
+        <Text style={styles.nonews}>No news available</Text>
+      ) : (
+        <FlatList
+          data={allnews}
+          renderItem={renderItem}
+          keyExtractor={(item, index) => `${item}-${index}`}
+        />
+      )}
     </SafeAreaView>
+    </GestureHandlerRootView>
   );
 }
 
@@ -38,4 +58,15 @@ const styles = StyleSheet.create({
   footer: { height: 50, backgroundColor: 'gray' },
   input: { borderWidth: 1, borderColor: 'black', padding: 10, margin: 10, borderRadius: 8},
   addbutton: {color: 'white', backgroundColor: 'blue', padding: 10, margin: 45, borderRadius: 8, alignItems: 'center' },
+  nonews: { textAlign: 'center', marginTop: 20, color: 'gray' },
+   taskRow: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    padding: 12,
+    backgroundColor: "white",
+    borderBottomWidth: 1,
+    borderBottomColor: "grey",
+  },
+  taskText: { fontSize: 16 }
 })
